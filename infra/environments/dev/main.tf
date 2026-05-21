fbec5b96a3d03e6381b55f3ca40ead7575c1cd41
hassansharif15 <hassansharif15@gmail.com>
2026-05-21 17:35:52 +0100

    changes to enviroments and add provider.tf

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -1,20 +1,10 @@
 terraform {
+  required_version = ">= 1.10.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-
-  default_tags {
-    tags = {
-      Project     = var.project
-      Environment = var.environment
-      ManagedBy   = "terraform"
     }
   }
 }
--- infra/environments/prod/backend.tf
+++ infra/environments/prod/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "memos-tfstate-devopsbyhassan-eu-west-2"
-    key            = "memos/prod/terraform.tfstate"
-    region         = "eu-west-2"
-    dynamodb_table = "memos-terraform-locks"
-    encrypt        = true
+    bucket       = "memos-tfstate-devopsbyhassan-eu-west-2"
+    key          = "memos/prod/terraform.tfstate"
+    region       = "eu-west-2"
+    encrypt      = true
+    use_lockfile = true
   }
 }
